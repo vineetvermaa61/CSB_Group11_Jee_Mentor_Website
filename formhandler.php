efbe220becf879f3520000d3e9aad1e1e2196374
VINEET KUMAR VERMA <vineetvermaa61@gmail.com>
2022-04-09 10:50:25 +0530

    Sample Commit

---
+++ formhandler.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+// --info@yourwebsite.com----
+
+$email_from = 'vineetvermaa61@gmail.com'  
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+                "User email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message.\n";
+
+$to = 'vineetvermaa61@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers = "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: Contact.html");
+
+
+
+
+?>
